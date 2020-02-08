6ffa5ffd6d9214bc7556f8d31fa493c44d52cbbf
P4NTY <49764816+P4NTY@users.noreply.github.com>
2020-02-08 21:42:49 +0100

    Create dbase.js

---
+++ src/Utils/dbase.js
@@ -0,0 +1,28 @@
+export const apiURL = 'https://api-euwest.graphcms.com/v1/ck112o9861rbg01e8636r8atb/master';
+
+export const getProjects = () => {
+    const request = fetch(
+
+    ).then(
+        //OK
+
+    ).catch(
+        //ERR
+    )
+
+    return request;
+}
+
+
+export const getTech = () => {
+    const request = fetch(
+        
+    ).then(
+        //OK
+
+    ).catch(
+        //ERR
+    )
+
+    return request;
+}
