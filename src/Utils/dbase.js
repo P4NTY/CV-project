162c5152c04affc1226be9ce38d38264eab4427c
P4NTY <49764816+P4NTY@users.noreply.github.com>
2020-02-09 18:39:05 +0100

    Update dbase.js

--- src/Utils/dbase.js
+++ src/Utils/dbase.js
@@ -1,28 +1,65 @@
 export const apiURL = 'https://api-euwest.graphcms.com/v1/ck112o9861rbg01e8636r8atb/master';
 
+const queryProjects = `{
+    projectses{
+      order
+      tittle
+      about {
+        html
+      }
+         startDate
+      endDate
+      teches {
+        name
+      }
+    }
+  }`;
+
+  const queryTechs = `{
+    teches {
+      name
+      icon {
+        url
+      }
+      link
+    }
+  };`
+
 export const getProjects = () => {
-    const request = fetch(
-
-    ).then(
-        //OK
-
-    ).catch(
-        //ERR
-    )
+    const request = fetch(apiURL, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: `{"query": ${queryProjects}}`,
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
 
     return request;
 }
 
 
 export const getTech = () => {
-    const request = fetch(
-        
-    ).then(
-        //OK
-
-    ).catch(
-        //ERR
-    )
+    const request = fetch(apiURL, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: `{"query": ${queryTechs}}`,
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
 
     return request;
 }
