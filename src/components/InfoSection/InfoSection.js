0ed19d83ecc86aefb6bbecd8d6cb02d44762239b
P4NTY <49764816+P4NTY@users.noreply.github.com>
2020-08-31 10:58:24 +0200

    Update InfoSection.js

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -5,7 +5,7 @@ const InfoSection = ({see}) => (
     <div className={`${style.wrapper} ${see && style.see}`}>
         <h1>Cześć,</h1>
         <h3>Strona ta przedstawia zarys moich doświadczeń, wyglądem ma przypominać zwykłe, papierowe CV, ale wzbogacone o dodatkowe funkcje dzięki wykorzystaniu przeglądarki. Cały czas rozwijam tą stronę i mam parę rzeczy do wdrożenia, jednak efekektem już chciałbym się podzelić ze światem.</h3>
-        <h3>Kod można podejżeć na&nbsp;<a href="https://github.com/P4NTY/CV-project" target="_blank" rel="noopener noreferrer">githubie</a>.</h3>
+        <h3>Strona została zrobiona w technologii React, a kod można podejżeć na&nbsp;<a href="https://github.com/P4NTY/CV-project" target="_blank" rel="noopener noreferrer">githubie</a>.</h3>
         <h3>Jeżeli zainteresowało cię to co się tutaj znajduje, bądź chciałbyś się ze mną skontaktować formularz kontaktowy kryję się pod ikonką listu w pasku po lewej. Jestem wdzięczny za każdą opinię jak i chęć kontaktu. W miarę możliwości staram się odpowiadać na każdego maila.</h3>
         <p>Miłego surfowania po sieci.</p>
     </div>
